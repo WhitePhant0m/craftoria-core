e7d43b392903d3fc5f6cf9330f99c3efc7190c69
Linguardium <61733861+Linguardium@users.noreply.github.com>
2025-06-10 19:04:05 -0500

    queue clientside stuff to render thread instead of directly removing the feature from XY

--- src/main/java/dev/wp/craftoria_core/mixin/xycraft/XyCoreClientMixin.java
+++ src/main/java/dev/wp/craftoria_core/mixin/xycraft/XyCoreClientMixin.java
@@ -1,18 +1,30 @@
 package dev.wp.craftoria_core.mixin.xycraft;
 
-import net.neoforged.bus.api.SubscribeEvent;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import dev.wp.craftoria_core.Craftoria;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
 import net.neoforged.neoforge.event.TagsUpdatedEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.Optional;
 
 @Mixin(targets = "tv.soaryn.xycraft.core.XyCoreClient$ForgeBus")
 public interface XyCoreClientMixin {
-    /**
-     * @author WP
-     * @reason Bad Xycraft, bad!
-     */
-    @SubscribeEvent
-    @Overwrite
-    static void onTagsUpdated(TagsUpdatedEvent event) {
+
+    @WrapMethod(method="onTagsUpdated")
+    private static void queueClientsideActionsOnClient(TagsUpdatedEvent event, Operation<Void> original) {
+        if (!Minecraft.getInstance().isSameThread()) {
+            Craftoria.LOGGER.warn("Queuing XYCraft onTagsUpdated clientside tasks to clientside thread executor");
+            Minecraft.getInstance().execute(()-> {
+                Craftoria.LOGGER.warn("Running XYCraft onTagsUpdated task on render thread instead of server thread");
+                Optional.ofNullable(Minecraft.getInstance().player)
+                        .map(Entity::registryAccess)
+                        .ifPresent(registry -> {
+                            original.call( new TagsUpdatedEvent(registry, false, true));
+                        });
+            });
+        }
     }
 }
