ba51c7a905bdbcbfc4cdbf55bf40f5129b76b57d
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-06-25 20:40:11 +0300

    suppress disconnect errors with unknown packet
    they just spam the log, and from what I've seen, they're useless errors(at least on server side)

---
+++ src/main/java/dev/wp/craftoria_core/mixin/minecraft/IdDispatchCodecMixin.java
@@ -0,0 +1,24 @@
+package dev.wp.craftoria_core.mixin.minecraft;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Cancellable;
+import com.llamalad7.mixinextras.sugar.Local;
+import dev.wp.craftoria_core.Craftoria;
+import net.minecraft.network.codec.IdDispatchCodec;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(IdDispatchCodec.class)
+public class IdDispatchCodecMixin {
+    @ModifyExpressionValue(method = "encode(Lio/netty/buffer/ByteBuf;Ljava/lang/Object;)V", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2IntMap;getOrDefault(Ljava/lang/Object;I)I"))
+    private int shutUp(int original, @Cancellable CallbackInfo ci) {
+        // Too much of it in our logs, and I can't find the source of it. Suppressing it.
+        // TODO: make this configurable, and off by default
+        if (original == -1) {
+            ci.cancel();
+            Craftoria.LOGGER.debug("Suppressed disconnect error: Unknown packet ID -1");
+        }
+        return original;
+    }
+}
